8075951279efaa26d495608e13cd0e60115e3c80
LevanPertenava <levan.pertenava1@gmail.com>
2021-09-01 19:50:46 +0400

    Add project files.

---
+++ CustomDependencyInjection/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CustomDependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBuilder<ServiceConsumer> serviceBuilder = new ServiceBuilder<ServiceConsumer>();
+            var service = serviceBuilder.GetService();
+        }
+    }
+
+    public class ServiceBuilder<T> where T : class
+    {
+        public T GetService()
+        {
+            return (T)GetService(typeof(T));
+        }
+        private object GetService(Type type)
+        {
+            var constructor = type.GetConstructors().Single();
+            var parameters = constructor.GetParameters();
+
+            if (parameters.Length > 0)
+            {
+                var parameterImplementations = new object[parameters.Length];
+
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    parameterImplementations[i] = GetService(parameters[i].ParameterType);
+                }
+                return Activator.CreateInstance(type, parameterImplementations);
+            }
+            return Activator.CreateInstance(type);
+        }
+    }
+
+    public class ServiceConsumer
+    {
+        private readonly HelloService _hello;
+        public ServiceConsumer(HelloService hello)
+        {
+            _hello = hello;
+            Console.WriteLine(_hello.Message());
+        }
+    }
+
+    public class HelloService
+    {
+        private readonly MessageService _message;
+        public HelloService(MessageService message)
+        {
+            _message = message;
+            Console.WriteLine(_message.Message());
+        }
+        public string Message()
+        {
+            return "Hello";
+        }
+    }
+
+    public class MessageService
+    {
+        public string Message()
+        {
+            return "Yo";
+        }
+    }
+}
